415acd74b7b79cd98600cbd99fc08588117aa5df
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-01 22:04:35 -0500

    Commit 6: Displayed users with Angular

--- datingApp_frontend/public/app.js
+++ datingApp_frontend/public/app.js
@@ -4,12 +4,23 @@ var app = angular.module('dating_app', []);
 
 app.controller('mainController', ['$http', function($http){
     this.message = "dating app ready to go";
+    this.daters = [];
 
     $http({
         method: 'GET',
         url: 'http://localhost:3000/users'
     }).then(function(response) {
         console.log(response);
-    });
+        this.daters = response.data;
+        console.log(this.daters);
+
+        var datersLeft = response.data.slice(0,3);
+        this.datersLeft = datersLeft;
+        console.log(datersLeft);
+
+        var datersRight = response.data.slice(3);
+        this.datersRight = datersRight;
+        console.log(datersRight);
+    }.bind(this));
 
 }])
